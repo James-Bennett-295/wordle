a568f7d5919f1a0c9534c85d772d1d4772e5396c
James Bennett <JamesBennett25565@gmail.com>
2022-03-09 22:14:22 +0000

    make so web app is opened automatically

--- wordle.js
+++ wordle.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const app = express();
+"use strict";
 
-app.use(express.static(__dirname + "/web/"));
+const express = require("express");
+const open = require("open");
+const app = express();
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/web/main.html");
-})
+});
+
+app.use(express.static(__dirname + "/web/"));
 
 app.listen(3000, () => {
     console.log("Listening on http://127.0.0.1:3000");
+    open("http://127.0.0.1:3000/");
 });
